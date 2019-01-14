a252048b17c1a4b551de4b3368298c6871b2ec51
Prihan Nimsara <Iamprihan@gmail.com>
2019-01-14 17:42:24 +0530

    stage one

---
+++ project/src/Sample/__init__.py
@@ -0,0 +1,39 @@
+variableOne = 10
+variableTwo = 20.0
+variableThree = "prihan"
+
+print(type(variableOne))
+print(type(variableTwo))
+print(type(variableThree))
+
+# Numbers
+
+# int
+variableOne = 10
+variableTwo = 20
+print(variableOne)
+print(variableTwo)
+
+# long
+variableThree = 908090800000000000000000
+print(variableThree)
+
+# float
+variableOne = 8.345
+print(variableOne)
+
+# complex
+variableTwo = 2.34j
+print(variableTwo)
+
+# String
+
+variableOne = "prihan"
+variableTwo = 'nimsara'
+
+print(variableOne)
+print(variableTwo)
+print(variableOne[0:2])
+print(variableOne[2])
+print(variableOne * 3)
+print(variableOne + variableTwo)
