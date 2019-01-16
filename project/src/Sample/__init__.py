2f5805526f28bb66531ea59d114541f53a657b38
Prihan Nimsara <Iamprihan@gmail.com>
2019-01-16 23:10:24 +0530

    final

--- project/src/Sample/__init__.py
+++ project/src/Sample/__init__.py
@@ -7,33 +7,52 @@ print(type(variableTwo))
 print(type(variableThree))
 
 # Numbers
-
 # int
 variableOne = 10
 variableTwo = 20
 print(variableOne)
 print(variableTwo)
-
 # long
 variableThree = 908090800000000000000000
 print(variableThree)
-
 # float
 variableOne = 8.345
 print(variableOne)
-
 # complex
 variableTwo = 2.34j
 print(variableTwo)
 
 # String
-
 variableOne = "prihan"
 variableTwo = 'nimsara'
-
 print(variableOne)
 print(variableTwo)
 print(variableOne[0:2])
 print(variableOne[2])
 print(variableOne * 3)
 print(variableOne + variableTwo)
+
+# List
+variableOne = ["prihan",1,2,"nimsara"]
+print(variableOne)
+print(variableOne[2:])
+print(variableOne + variableOne)
+print(variableOne * 3)
+
+# Tuple
+# tuple we can't modify just read only.size is already fixed at declare time 
+variableTwo = ('prihan',1,2,'nimsara')
+print(type(variableTwo[2]))
+print(variableTwo[1:])
+print(variableTwo[1])
+print(variableTwo+variableTwo)
+print(variableTwo*3)
+
+# Dictionary
+variableThree = {1:"prihan",2:"nisara",3:"buddhika",4:2345}
+print(variableThree[1])
+print(variableThree[4])
+print(variableThree)
+print(variableThree.keys())
+print(variableThree.values())
+
